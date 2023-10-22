7ccfe681a77daad16e15842fe5103d5f28295a2c
Ketok <ketok4321@gmail.com>
2023-10-22 10:30:17 +0200

    Generate more stuff

--- home/adv/generated/gen.sh
+++ home/adv/generated/gen.sh
@@ -2,5 +2,5 @@
 base=$(dirname "$0")
 
 echo "Generating dependencies... (this should take a few seconds)"
-adv gen class_number -n 100 -o "${base}/class_number.adv"
-adv gen array -n 8 -o "${base}/array.adv"
+adv gen class_number -n 1000 -o "${base}/class_number.adv"
+adv gen array -n 16 -o "${base}/array.adv"
